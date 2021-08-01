379896ba77690774186979a542710816ee6dfd62
Байгалов Каныш <kbaigalov@alseco.kz>
2021-08-01 16:35:44 +0600

    Домашнее задание №1.

---
+++ L01-gradle/src/main/java/HelloOtus.java
@@ -0,0 +1,13 @@
+import com.google.common.base.Predicates;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
+public class HelloOtus {
+
+    public static void main(String[] args) {
+        List<String> values = Lists.newArrayList("a", null, "b", "c");
+        Iterable<String> withoutNulls = Iterables.filter(values, Predicates.notNull());
+    }
+}
