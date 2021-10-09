ae857b7c0d9df1b10fe5247044f3fd1b9ac0d248
JavaProgrammist <kbaigalov@alseco.kz>
2021-10-09 16:32:26 +0600

    Выполнение домашнего задания урока №10: изменения.

--- L10-byte-codes/src/main/java/ru/otus/TestLoggingImpl.java
+++ L10-byte-codes/src/main/java/ru/otus/TestLoggingImpl.java
@@ -4,23 +4,26 @@ public class TestLoggingImpl implements TestLogging {
     @Log
     @Override
     public void calculation(int param) {
-
+        System.out.println("in calculation(int param)");
     }
 
     @Log
     @Override
     public void calculation(int param1, int param2) {
+        System.out.println("in calculation(int param1, int param2)");
 
     }
 
     @Override
     public void calculation(int param1, double param2) {
+        System.out.println("in calculation(int param1, double param2)");
 
     }
 
     @Log
     @Override
     public void calculation(int param1, int param2, String param3) {
+        System.out.println("in calculation(int param1, int param2, String param3)");
 
     }
 }
